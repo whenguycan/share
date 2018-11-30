39a5271c7ba23b069512f8325afda3201341e33f
jim <jim@cit.com>
2018-11-30 17:14:50 +0800

    update

--- DNFTest1.java
+++ DNFTest1.java
@@ -1,13 +1,12 @@
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * 
@@ -24,13 +23,14 @@ public class DNFTest1 {
 		try {
 			JFrame mainFrame = new JFrame();
 			mainFrame.setBounds(200, 200, 800, 600);
+			mainFrame.setBackground(Color.WHITE);
 
 			JComponentManager.bind(mainFrame);
 
 			mainFrame.addWindowListener(new WindowAdapter() {
 
 				public void windowClosing(WindowEvent e) {
-					super.windowClosing(e);
+					System.exit(0);
 				}
 			});
 			mainFrame.setVisible(true);
@@ -42,200 +42,242 @@ public class DNFTest1 {
 	static class JComponentManager {
 
 		static Dimension dim = new Dimension(100, 25);
+		static Point leftTop = new Point(0, 0);
+		static Point left2Top = new Point(300, 0);
+		static Point left3Top = new Point(0, 200);
 
 		static void bind(JFrame mainFrame) {
 			if (mainFrame == null)
 				return;
 			mainFrame.setLayout(null);
 
-			JComboBox<Item> touJianSel = new JComboBox<>(getItemList(ItemType.TouJian));
-			touJianSel.setBounds(0, 0, dim.width, dim.height);
+            JComboBox<String> comboJian = new JComboBox<>(new String[]{"Jian"});
+            comboJian.setBounds(leftTop.x, leftTop.y, dim.width, dim.height);
+            mainFrame.add(comboJian);
 
-			mainFrame.add(touJianSel);
+            JComboBox<String> comboTui = new JComboBox<>(new String[]{"Tui"});
+            comboTui.setBounds(leftTop.x, leftTop.y + dim.height, dim.width, dim.height);
+            mainFrame.add(comboTui);
+
+            JComboBox<String> comboXie = new JComboBox<>(new String[]{"Xie"});
+            comboXie.setBounds(leftTop.x, leftTop.y + dim.height * 2, dim.width, dim.height);
+            mainFrame.add(comboXie);
+
+            JComboBox<String> comboXiong = new JComboBox<>(new String[]{"Xiong"});
+            comboXiong.setBounds(leftTop.x + dim.width, leftTop.y, dim.width, dim.height);
+            mainFrame.add(comboXiong);
+
+            JComboBox<String> comboYao = new JComboBox<>(new String[]{"Yao"});
+            comboYao.setBounds(leftTop.x + dim.width, leftTop.y + dim.height, dim.width, dim.height);
+            mainFrame.add(comboYao);
+
+            JComboBox<String> comboWuQi = new JComboBox<>(new String[]{"WuQi"});
+            comboWuQi.setBounds(left2Top.x, left2Top.y, dim.width, dim.height);
+            mainFrame.add(comboWuQi);
+
+            JComboBox<String> comboShouZhuo = new JComboBox<>(new String[]{"ShouZhuo"});
+            comboShouZhuo.setBounds(left2Top.x, left2Top.y + dim.height, dim.width, dim.height);
+            mainFrame.add(comboShouZhuo);
+
+            JComboBox<String> comboFuZhu = new JComboBox<>(new String[]{"FuZhu"});
+            comboFuZhu.setBounds(left2Top.x, left2Top.y + dim.height * 2, dim.width, dim.height);
+            mainFrame.add(comboFuZhu);
+
+            JComboBox<String> comboErHuan = new JComboBox<>(new String[]{"ErHuan"});
+            comboErHuan.setBounds(left2Top.x, left2Top.y + dim.height * 3, dim.width, dim.height);
+            mainFrame.add(comboErHuan);
+
+            JComboBox<String> comboChengHao = new JComboBox<>(new String[]{"ChengHao"});
+            comboChengHao.setBounds(left2Top.x + dim.width, left2Top.y, dim.width, dim.height);
+            mainFrame.add(comboChengHao);
+
+            JComboBox<String> comboXiangLian = new JComboBox<>(new String[]{"XiangLian"});
+            comboXiangLian.setBounds(left2Top.x + dim.width, left2Top.y + dim.height, dim.width, dim.height);
+            mainFrame.add(comboXiangLian);
+
+            JComboBox<String> comboJieZhi = new JComboBox<>(new String[]{"JieZhi"});
+            comboJieZhi.setBounds(left2Top.x + dim.width, left2Top.y + dim.height * 2, dim.width, dim.height);
+            mainFrame.add(comboJieZhi);
+
+            JComboBox<String> comboMoFaShi = new JComboBox<>(new String[]{"MoFaShi"});
+            comboMoFaShi.setBounds(left2Top.x + dim.width, left2Top.y + dim.height * 3, dim.width, dim.height);
+            mainFrame.add(comboMoFaShi);
+
+            JTextField textBasic = new JTextField("111");
+            textBasic.setBounds(left3Top.x, left3Top.y, dim.width, dim.height);
+            textBasic.setEditable(false);
+            textBasic.setHorizontalAlignment(JTextField.RIGHT);
+            mainFrame.add(textBasic);
+
+            JButton btnCalc = new JButton("Calc");
+            btnCalc.setBounds(left3Top.x + dim.width, left3Top.y, dim.width, dim.height);
+            mainFrame.add(btnCalc);
+
+            JTextField textDamage = new JTextField();
+            textDamage.setBounds(left3Top.x + dim.width * 2, left3Top.y, dim.width, dim.height);
+            textDamage.setEditable(false);
+            textDamage.setHorizontalAlignment(JTextField.RIGHT);
+            mainFrame.add(textDamage);
+
+            btnCalc.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    GameRole gr = new GameRole("", 2000, 13, 1000);
+                    int damage = calc(gr);
+                    textDamage.setText("" + damage);
+                }
+            });
 		}
 	}
 
-	static int calc(Role role, Item... items) {
-		int basic = 1;
+    static int calc(GameRole gr, Item... items) {
+        int basic = 1;
+        int damage = 0;
 
-		return basic;
-	}
+        return damage;
+    }
+
+    static class GameRole extends BaseAttrs {
+
+	    public GameRole(String name, int inte, int attr, int inde) {
+            this.name = name;
+            this.inte = inte;
+            this.attr = attr;
+            this.inde = inde;
+        }
+
+    }
 
 	enum ItemType {
-		TouJian, ShangYi, XiaZhuang, YaoDai, Xie, WuQi, ChengHao, ShouZhuo, XiangLian, FuZhu, JieZhi, ErHuan, MoFaShi
+        Jian, Xiong, Tui, Yao, Xie,
+        WuQi, ChengHao,
+        ShouZhuo, XiangLian, FuZhu, JieZhi, ErHuan, MoFaShi,
+        TaoZhuang;
 	}
 
-	enum ItemSuit {
-		MoZhan, WanShi, HuangHu, HaiBoLun;
-	}
+	static Item[] getItemList(ItemType itemType){
+        List<Item> list = new ArrayList<>();
+        switch (itemType){
+            case Jian:
+                list.add(Items.MoZhanJian);
+                break;
+            default:
+                break;
+        }
+        return list.toArray(new Item[list.size()]);
+    }
 
-	static class BaseAttrs {
-		String name;
-		int inte;
-		int intePerc;
-		int yell;
-		int whit;
-		int crit;
-		int yellEx;
-		int critEx;
-		int thre;
-		int fina;
-		int skil;
-		int attr;
-		int inde;
-		public BaseAttrs() {
+    interface Items {
+        Item MoZhanJian = new Item("", ItemType.Jian);
+	    Item YaoDao = new Item("", ItemType.WuQi).inte(112).inde(1157).yellEx(35).thre(22);
+    }
 
-		}
-		public BaseAttrs(String name) {
-			this.name = name;
-		}
-		BaseAttrs load(String info) {
-			if(info == null)
-				throw new RuntimeException("info is null");
-			String[] arr = info.split("-");
-			if(arr.length != 12)
-				throw new RuntimeException("arr length is not 12");
-			this.inte = Integer.parseInt(arr[0]);
-			this.intePerc = Integer.parseInt(arr[1]);
-			this.yell = Integer.parseInt(arr[2]);
-			this.whit = Integer.parseInt(arr[3]);
-			this.crit = Integer.parseInt(arr[4]);
-			this.yellEx = Integer.parseInt(arr[5]);
-			this.critEx = Integer.parseInt(arr[6]);
-			this.thre = Integer.parseInt(arr[7]);
-			this.fina = Integer.parseInt(arr[8]);
-			this.skil = Integer.parseInt(arr[9]);
-			this.attr = Integer.parseInt(arr[10]);
-			this.inde = Integer.parseInt(arr[11]);
-			return this;
-		}
-	}
+    interface Suits {
+        Item MoZhan5 = new Item("", ItemType.TaoZhuang);
+        Item WanShi3 = new Item("", ItemType.TaoZhuang);
+        Item WanShi5 = new Item("", ItemType.TaoZhuang);
+        Item HuangHu3 = new Item("", ItemType.TaoZhuang);
+        Item HaiBoLun3JiGong = new Item("", ItemType.TaoZhuang);
+        Item HaiBoLun3ShuBai = new Item("", ItemType.TaoZhuang);
+    }
 
-	static class Role extends BaseAttrs {
-		static Role XiuLuo;
-		static {
-			XiuLuo = (Role) new BaseAttrs("修罗").load(Infos.XiuLuo);
-		}
-	}
+    static class Item extends BaseAttrs {
 
-	static class Infos {
-		static String XiuLuo = "";
-		static String MoZhan5 = "";
-		static String WanShi3 = "";
-		static String WanShi5 = "";
-		static String HuangHu3 = "";
-		static String HaiBoLun3 = "";
-		static String MoZhanJian = "";
-		static String WanShiJian = "";
-		static String MoZhanShangYi = "";
-		static String WanShiShangYi = "";
-		static String MoZhanXiaZhuang = "";
-		static String WanShiXiaZhuang = "";
-		static String MoZhanYaoDai = "";
-		static String WanShiYaoDai = "";
-		static String MoZhanXie = "";
-		static String WanShiXie = "";
-		static String YaoDao = "";
-		static String AnYing = "";
-		static String LongZhiTiaoZhan = "";
-		static String WuJinShouZhuo = "";
-		static String QiMingXing = "";
-		static String WuJinXiangLian = "";
-		static String YinYun = "";
-		static String HeiBaiJiaMian = "";
-		static String WangGuanFeiGuan = "";
-		static String WuJinJieZhi = "";
-		static String QingQuan = "";
-		static String YingXiongWang = "";
-		static String HeiAnJiLi = "";
-		static String HeiBaiLingHun = "";
-		static String WangZuoBenYuan = "";
-	}
+        ItemType itemType;
 
-	static class Item extends BaseAttrs {
+        public Item(String name, ItemType itemType) {
+            this.name = name;
+            this.itemType = itemType;
+        }
 
-		ItemType itemType;
-		ItemSuit itemSuit;
+        Item inte(int inte){
+            this.inte = inte;
+            return this;
+        }
 
-		public static Item build(String name, ItemType itemType, ItemSuit itemSuit){
-			Item item = new Item();
-			item.name = name;
-			item.itemType = itemType;
-			item.itemSuit = itemSuit;
-			return item;
-		}
+        Item attr(int attr){
+            this.attr = attr;
+            return this;
+        }
 
-	}
+        Item inde(int inde){
+            this.inde = inde;
+            return this;
+        }
 
-	static Map<String, BaseAttrs> getSuitMap (){
-		Map<String, BaseAttrs> map = new HashMap<>();
-		map.put(ItemSuit.MoZhan.name() + "5", new BaseAttrs("魔战5").load(Infos.MoZhan5));
-		map.put(ItemSuit.WanShi.name() + "3", new BaseAttrs("万世3").load(Infos.WanShi3));
-		map.put(ItemSuit.WanShi.name() + "5", new BaseAttrs("万世5").load(Infos.WanShi5));
-		map.put(ItemSuit.HuangHu.name() + "3", new BaseAttrs("恍惚3").load(Infos.HuangHu3));
-		map.put(ItemSuit.HaiBoLun.name() + "3", new BaseAttrs("海伯伦3").load(Infos.HaiBoLun3));
-		return map;
-	}
+        Item yell(int yell){
+            this.yell = yell;
+            return this;
+        }
 
-	static Item[] getItemList(ItemType itemType) {
-		List<Item> list = new ArrayList<>();
+        Item whit(int whit){
+            this.whit = whit;
+            return this;
+        }
 
-		switch (itemType) {
-			case TouJian:
-				list.add(Item.build("魔战肩", ItemType.TouJian, ItemSuit.MoZhan).attrs(34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("万世肩", ItemType.TouJian, ItemSuit.WanShi).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case ShangYi:
-				list.add(Item.build("魔战胸", ItemType.ShangYi, ItemSuit.MoZhan).attrs(43, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("万世胸", ItemType.ShangYi, ItemSuit.WanShi).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case XiaZhuang:
-				list.add(Item.build("魔战腿", ItemType.XiaZhuang, ItemSuit.MoZhan).attrs(43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("万世腿", ItemType.XiaZhuang, ItemSuit.WanShi).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case YaoDai:
-				list.add(Item.build("魔战腰", ItemType.YaoDai, ItemSuit.MoZhan).attrs(25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("万世腰", ItemType.YaoDai, ItemSuit.WanShi).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case Xie:
-				list.add(Item.build("魔战鞋", ItemType.Xie, ItemSuit.MoZhan).attrs(113, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0));
-				list.add(Item.build("万世鞋", ItemType.Xie, ItemSuit.WanShi).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case WuQi:
-				list.add(Item.build("妖刀", ItemType.WuQi, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("暗影", ItemType.WuQi, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case ChengHao:
-				list.add(Item.build("龙之挑战", ItemType.ChengHao, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case ShouZhuo:
-				list.add(Item.build("大无尽", ItemType.ShouZhuo, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("启明星", ItemType.ShouZhuo, ItemSuit.HuangHu).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case XiangLian:
-				list.add(Item.build("大无尽", ItemType.XiangLian, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("氤氲", ItemType.XiangLian, ItemSuit.HuangHu).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case FuZhu:
-				list.add(Item.build("假面", ItemType.TouJian, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("王冠非冠", ItemType.TouJian, ItemSuit.HaiBoLun).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case JieZhi:
-				list.add(Item.build("大无尽", ItemType.TouJian, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("清泉", ItemType.TouJian, ItemSuit.HuangHu).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case ErHuan:
-				list.add(Item.build("英雄王", ItemType.TouJian, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("黑暗祭礼", ItemType.TouJian, ItemSuit.HaiBoLun).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			case MoFaShi:
-				list.add(Item.build("黑白", ItemType.TouJian, null).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				list.add(Item.build("王座本源", ItemType.TouJian, ItemSuit.HaiBoLun).attrs(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-				break;
-			default:
-				break;
-		}
-		return list.toArray(new Item[list.size()]);
-	}
+        Item crit(int crit){
+            this.crit = crit;
+            return this;
+        }
+
+        Item yellEx(int yellEx){
+            this.yellEx = yellEx;
+            return this;
+        }
+
+        Item whitEx(int whitEx){
+            this.whitEx = whitEx;
+            return this;
+        }
+
+        Item critEx(int critEx){
+            this.critEx = critEx;
+            return this;
+        }
+
+        Item thre(int thre){
+            this.thre = thre;
+            return this;
+        }
+
+        Item fina(int fina){
+            this.fina = fina;
+            return this;
+        }
+
+        Item skil(int skil){
+            this.skil = skil;
+            return this;
+        }
+
+        Item intePrec(int intePrec){
+            this.intePrec = intePrec;
+            return this;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+    }
+
+    static class BaseAttrs {
+
+        String name;
+        int inte;
+        int attr;
+        int inde;
+
+        int yell;
+        int whit;
+        int crit;
+        int yellEx;
+        int whitEx;
+        int critEx;
+
+        int thre;
+        int fina;
+        int skil;
+        int intePrec;
+
+    }
 
 }
